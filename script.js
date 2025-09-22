cc2cc970eed31f6231e6a4bf4e5fc7a5b9c371c5
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2025-09-22 16:09:13 -0300

    Lousa desenhando com o segurar do click

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-const lousa = document.getElementById('lousa')
+const lousa = document.getElementById('lousa');
+const gridButton = document.getElementById('grid-btn');
+const titulo = document.querySelector('h1');
 
-const grid = 16;
-const lousa_Height_Width = `${(900 / grid)}px`;
+let clickAndHold = false;
+lousa.addEventListener('mouseleave', (e) => {
+        e.preventDefault()
+        clickAndHold = false; 
+}); 
+lousa.addEventListener('mousedown', (e) => {
+        e.preventDefault()
+        clickAndHold = true; 
+        e.target.classList.add('background-black')
+});
+lousa.addEventListener('mouseup', (e) => {
+        e.preventDefault()
+        clickAndHold = false; 
+});
 
-for(let l = 1; l <= grid; l++){
-    for(let c = 1; c <= grid; c++){
-        const div = document.createElement('div');
-        div.style.width = lousa_Height_Width;
-        div.style.height = lousa_Height_Width;
+const criarLousar = (grid) => {
+    const lousa_Height_Width = `${(900 / grid)}px`;
 
-        div.classList.add('squares');
-        lousa.appendChild(div);
+    for(let l = 1; l <= grid; l++){
+        for(let c = 1; c <= grid; c++){
+            const div = document.createElement('div');
+
+            div.addEventListener('mouseout', (e) => {
+                e.preventDefault()
+                if(clickAndHold){
+                    e.target.classList.add('background-black')
+                }
+            })
+            
+            div.style.width = lousa_Height_Width;
+            div.style.height = lousa_Height_Width;
+            div.classList.add('quadrado');
+            lousa.appendChild(div);
+        }
     }
 }
 
-lousa.addEventListener('mouseup', (e) => {
-    e.preventDefault()
-
-    e.target.classList.add('background-black')
-})
+criarLousar(64);
